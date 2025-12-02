09085deae3e839032c413e905cc22495c3889040
Randy <imagine90359@yahoo.com.tw>
2025-12-02 11:36:43 +0000

    add

---
+++ app/pages/history.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref, reactive } from 'vue';
+import { getData } from 'nuxt-storage/local-storage';
+
+const route = useRoute()
+
+const currentPage = ref(1)
+const perPage = ref(12)
+
+const queryObj = reactive({
+  word: null,
+  ids: getData('menu-history') || null,
+  limit: perPage.value,
+	offset: perPage.value * (currentPage.value - 1),
+  sortBy: 'id',
+  orderBy: 'desc',
+})
+
+setQueryObj(route)
+const { data: list, refresh } = await useFetch('/api/menu', { query: queryObj, watch: false })
+const word = computed(() => route.query.word)
+const { data: totalData } = await useFetch('/api/menu/count', { 
+  query: { ids: queryObj.ids, word: word },
+})
+
+onBeforeRouteUpdate(async (to) => {
+	setQueryObj(to)
+  refresh()
+})
+
+function setQueryObj(route) {
+	if (route.query.word) queryObj.word = route.query.word
+	else delete queryObj.word
+	if (route.query.page) currentPage.value = Number(route.query.page)
+	else currentPage.value = 1
+	queryObj.offset = perPage.value * (currentPage.value - 1)
+}
+</script>
+
+<template>
+  <div class="row">
+    <h3>星號標記的菜單</h3>
+    <div v-if="list.length === 0">
+      目前沒有星號標記的菜單
+    </div>
+    <div class="col-6 col-md-4 col-lg-3" v-for="(item, key) in list" :key="key">
+      <NuxtLink :to="'/menu/' + item.id" class="text-decoration-none">
+        <div class="card mb-3">
+          <img v-if="item.first_img_filename" :src="'/static/image/' + item.id + '/' + item.first_img_filename" class="card-img-top" style="height:200px;object-fit: contain;">
+          <div v-else class="card-img-top bg-body-secondary d-flex align-items-center justify-content-center text-primary-emphasis" style="height:200px;">
+            No image
+          </div>
+          <div class="card-body" style="height:80px;">
+            <h5 class="card-title">{{ item.store }}</h5>
+          </div>
+        </div>
+      </NuxtLink>
+    </div>
+    <div class="d-flex justify-content-center">
+      <Pagination
+        v-model="currentPage"
+        :per-page="perPage"
+        :total-data="totalData"
+      ></Pagination>
+    </div>
+  </div>
+</template>
